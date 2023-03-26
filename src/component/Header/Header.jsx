d72afcd078b7488ec6f031fc6893c1e61e014723
saifularafat <saifularafat.info@gmail.com>
2023-03-27 00:59:36 +0600

    header section added

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="ema-john Logo" />
+            <div>
+                <a href="/Order">Order</a>
+                <a href="/Order Review">Order Review</a>
+                <a href="/Manage Inventory">Manage Inventory</a>
+                <a href="/login">login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
